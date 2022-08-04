946083d9d61dd3e3e83c333d4169cda27003cd40
Petyo Ivanov <underlog@gmail.com>
2022-08-04 09:30:03 +0300

    feat: track scroll top (pt1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import "./App.css";
 import * as u from "@virtuoso.dev/urx";
 import { systemToComponent } from "@virtuoso.dev/react-urx";
@@ -8,6 +8,7 @@ import { systemToComponent } from "@virtuoso.dev/react-urx";
 const listSystem = u.system(() => {
   const totalCount = u.statefulStream(0);
   const itemHeight = u.statefulStream(20);
+  const scrollTop = u.statefulStream(0);
 
   // let's calculate the items based on the two inputs from above.
   // note: recalculating the items array each time itemHeight changes is sub-optimal
@@ -28,29 +29,73 @@ const listSystem = u.system(() => {
 
   return {
     // inputs
+    // these two will come from component props...
     totalCount,
     itemHeight,
+    // from system POV, scrollTop is an input, just like the component props.
+    // It does not matter that it gets reported from the user input.
+    scrollTop,
 
     // output
     items,
   };
 });
 
+function useScrollTop(callback: (value: number) => void) {
+  const elRef = useRef<HTMLDivElement>();
+  const onScrollCallback = useCallback(
+    (e: Event) => {
+      callback((e.target as HTMLDivElement)?.scrollTop);
+    },
+    [callback]
+  );
+
+  const callbackRef = (el: HTMLDivElement | null) => {
+    if (el) {
+      el.addEventListener("scroll", onScrollCallback);
+      elRef.current = el;
+    } else {
+      elRef.current?.removeEventListener("scroll", onScrollCallback);
+    }
+  };
+
+  return callbackRef;
+}
+
 // this will be our React component root.
 const ListRoot = () => {
   const items = useEmitterValue("items");
+  const scrollTopPublisher = usePublisher("scrollTop");
+  const scrollTop = useEmitterValue("scrollTop");
+  // this is just for example purposes.
+  const ref = useScrollTop(scrollTopPublisher);
   return (
     <div>
-      {items.map((item) => (
-        <div style={{ height: item.height }}>{item.label}</div>
-      ))}
+      scrollTop: {scrollTop}
+      <div
+        ref={ref}
+        style={{
+          height: 300,
+          width: 500,
+          overflowY: "auto",
+          border: "1px solid black",
+        }}
+      >
+        {items.map((item) => (
+          <div style={{ height: item.height }}>{item.label}</div>
+        ))}
+      </div>
     </div>
   );
 };
 
 // systemToComponent constructs the actual, public component from the system and the component root.
 // the `useEmitterValue` is a hook used in the component root.
-const { Component: List, useEmitterValue } = systemToComponent(
+const {
+  Component: List,
+  useEmitterValue,
+  usePublisher,
+} = systemToComponent(
   listSystem,
   {
     required: {
@@ -62,7 +107,7 @@ const { Component: List, useEmitterValue } = systemToComponent(
 );
 
 function App() {
-  const [totalCount, setTotalCount] = useState(10);
+  const [totalCount, setTotalCount] = useState(100);
   const [itemHeight, setItemHeight] = useState(20);
   return (
     <div className="App">
